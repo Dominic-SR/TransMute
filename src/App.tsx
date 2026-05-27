66854f3558ec1afe3b61dcf93737020322225d4b
Dominic-SR <dominicseduraja3@gmail.com>
2026-05-27 23:39:20 +0530

    file changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Convert from './pages/Convert'
-import About from './pages/About'
-import Docs from './pages/Docs'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -13,9 +10,6 @@ function App() {
         <main className="pb-16">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/convert" element={<Convert />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/docs" element={<Docs />} />
           </Routes>
         </main>
       </div>
